30e22b5cb29be58de1cb3249304d45157ce7f183
Hyia <zs360@qq.com>
2018-03-17 20:48:46 +0800

    first commit

---
+++ src/main/java/cn/hssnow/dler/articlecore/ArticleCoreApplication.java
@@ -0,0 +1,12 @@
+package cn.hssnow.dler.articlecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleCoreApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleCoreApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/hssnow/dler/articlecore/ArticleCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.hssnow.dler.articlecore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArticleCoreApplicationTests {
+    
+    @Test
+    public void contextLoads() {
+    }
+    
+}
